fc332637d03e4cbef99a9c41d2e557d8e9a7af0d
mithhu <mithhu666@gmail.com>
2019-01-22 20:53:10 +0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+import { ButtonContainer } from "./Button";
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <Navwrapper className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        {/* 
+          https://www.iconfinder.com/icons/1243689/call_phone_icon
+          Creative Commons (Attribution 3.0 Unported);
+          https://www.iconfinder.com/Makoto_msk */}
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <ButtonContainer>
+            <i className="fas fa-cart-plus" />
+            my cart
+          </ButtonContainer>
+        </Link>
+      </Navwrapper>
+    );
+  }
+}
+
+const Navwrapper = styled.nav`
+  background: var(--mainBlue) !important;
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
+
+export default Navbar;
